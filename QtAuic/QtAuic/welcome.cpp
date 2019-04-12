d258ae8d5751e728049b047127a4ccd997f13a8b
Tony Jiang <44115200+skyfackr@users.noreply.github.com>
2019-04-13 02:27:22 +0800

    build welcome

---
+++ QtAuic/QtAuic/welcome.cpp
@@ -0,0 +1,23 @@
+/*本文件用来输出欢迎界面*/
+#include"pch.h"
+#include"welcome.h"
+#include"resource.h"
+#include<Windows.h>
+using namespace std;
+using namespace qtauic;
+#define welcomefile IDR_TXT4
+void qtauic::welcome()
+{
+	const LPCTSTR filename = MAKEINTRESOURCE(welcomefile);
+	HRSRC fp = FindResource(NULL, filename, TEXT("TXT"));
+	if (fp == NULL) return;
+	DWORD fsize = SizeofResource(NULL, fp);
+	if (fsize == 0) return;
+	HGLOBAL fmem = LoadResource(NULL, fp);
+	if (fmem == NULL) return;
+	LPVOID fmemp = LockResource(fmem);
+	if (fmemp == NULL) return;
+	cout.write((char*)fmem, fsize);
+	FreeResource(fmem);
+	return;
+}
---
+++ QtAuic/QtAuic/welcome.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef SKYFACKR_QTAUIC_WELCOME_H_
+/*本文件声明欢迎界面展示接口*/
+#define SKYFACKR_QTAUIC_WELCOME_H_
+#include"pch.h"
+
+namespace qtauic
+{
+	//展示欢迎界面
+	void welcome();
+}
+
+#endif // !SKYFACKR_QTAUIC_WELCOME_H_
